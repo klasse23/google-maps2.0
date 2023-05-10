1f96cc77758e09f5bf2589f79a2f6f1ef3a4b424
ApenJulius <julian.sjonfjell@gmail.com>
2023-05-10 18:34:57 +0200

    page constructed, missing text

--- pageContstructor.js
+++ pageContstructor.js
@@ -1,19 +1,17 @@
 const title = document.title.replace(" – Program Stoppested verden", "")
-let cat
 const attachmentPoint = document.getElementsByClassName("page-content")
 function getPageData(){
+    console.log("Starting")
     fetch("https://program.stoppestedverden.no/wp-content/plugins/Klasse23/pages.json")
      .then((response) => response.json())
      .then((data) => {
         for(let category of Object.keys(data["category"])){
             if(data["category"][category]["pages"][title] !== undefined){
-                cat=category
                 console.log("Found Correct Activity")
-                createPage(category, data["category"][category]["pages"][title])
+                createPage(category, data["category"][category]["pages"][title], data["category"][category].color)
                 break
             }
         }
-         console.log(cat)
      })
 }
 
@@ -21,8 +19,27 @@ getPageData()
 
 
 
-function createPage(category, pageData) {
-    console.log(category, pageData)
-    let l = document.createElement("div")
-    const newContainer = attachmentPoint[0].appendChild(document.createElement("div"));
+function createPage(category, pageData, color) {
+    
+    console.log(pageData["Land"],category, pageData)
+    const newContainer = document.createElement("div");
+    newContainer.style = "display:block;"
+    newContainer.innerHTML = 
+    `
+    <img src="${pageData["1280x844"]}" class="cover-bilde">
+    <button class="Kategori-knapp" style="background-color:${color};">${category}</button>
+    <h4 class="Land">${pageData.Land}</h4>
+    <h1 class="Side-Tittel">${title}</h1>
+    <audio class="audio-player"></audio>
+    <p class="page-content"></p>
+    <link rel="stylesheet" type="text/css" href="https://program.stoppestedverden.no/wp-content/plugins/Klasse23/style.css" />`
+    attachmentPoint[0].appendChild(newContainer);
+
+    
+}
+
+
+function addText(){
+    fetch(`https://program.stoppestedverden.no/wp-content/plugins/Klasse23/`)
+ 
 }
